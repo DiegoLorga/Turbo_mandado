12a32eab46d0746581a5be29c7bf054613868543
KarlaZur <108319861+KarlaZur@users.noreply.github.com>
2025-05-21 17:36:04 -0600

    Karla Dasbord

--- TurboMandado/settings.py
+++ TurboMandado/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login_registro',
-    'dashboard'
+    'dashboard',
+     'django_extensions',
 ]
 
 MIDDLEWARE = [
